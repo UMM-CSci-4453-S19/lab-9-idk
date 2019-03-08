db2f9ef92b6a6d78d7a57c90356286f68586bd6b
stev0531 <stev0531@morris.umn.edu>
2019-03-08 16:37:38 -0600

    Start button functionality

--- express.js
+++ express.js
@@ -1,19 +1,16 @@
+Promise = require('bluebird');
+mysql = require('mysql');
+DBF = require('./dbf-setup.js');
+
 var express=require('express'),
-mysql=require('mysql'),
-credentials=require('./credentials.json'),
 app = express(),
 port = process.env.PORT || 1337;
 
-credentials.host='ids.morris.umn.edu'; //setup database credentials
-
-var connection = mysql.createConnection(credentials); // setup the connection
-
-connection.connect(function(err){if(err){console.log(error)}});
 
 app.use(express.static(__dirname + '/public'));
 app.get("/buttons",function(req,res){
-  var sql = 'SELECT * FROM test.till_buttons';
-  connection.query(sql,(function(res){return function(err,rows,fields){
+  var sql = 'SELECT * FROM forever_alone.till_buttons';
+  DBF.query(sql,(function(res){return function(err,rows,fields){
      if(err){console.log("We have an error:");
              console.log(err);}
      res.send(rows);
@@ -21,13 +18,17 @@ app.get("/buttons",function(req,res){
 });
 app.get("/click",function(req,res){
   var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
+  var sql = 'select price from forever_alone.prices where prices.id=' + id;
   console.log("Attempting sql ->"+sql+"<-");
 
-  connection.query(sql,(function(res){return function(err,rows,fields){
+  var result = DBF.query(mysql.format(sql));
+
+
+
+  DBF.query(sql,(function(res){return function(err,rows,fields){
      if(err){console.log("We have an insertion error:");
              console.log(err);}
-     res.send(err); // Let the upstream guy know how it went
+     res.send(result); // Let the upstream guy know how it went
   }})(res));
 });
 // Your other API handlers go here!
